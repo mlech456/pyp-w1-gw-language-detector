2a87cb917af8da225b7a2185412396664d43c453
Mark <marklechmanik@gmail.com>
2016-08-24 01:34:15 +0000

    Fixed comments

--- language_detector/main.py
+++ language_detector/main.py
@@ -6,18 +6,16 @@ from languages import LANGUAGES
 
 
 def detect_language(text, languages=LANGUAGES):
-    
-    stats = {}
+    stats = {}                          #Creating a dynamic counter
     for language in languages:
         name = language['name']
         stats[name] = 0
     
-    for item in text.split(' '):    #iterate through each word in text
-        for language in languages:  #iterates through each language
+    for item in text.split(' '):        #iterates through each word in text
+        for language in languages:      #iterates through each language
             name = language['name']
             for word in language.get('common_words'): #iterates through each common word
-                # language
                 if item == word:
-                    stats[name] += 1
+                    stats[name] += 1    #Adds to the dynamic counter
     
     return max(stats, key=stats.get)

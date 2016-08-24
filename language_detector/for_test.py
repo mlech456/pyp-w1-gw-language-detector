637a1a3d1093e21cd7a08093e7b41fb0ceed3193
Mark <marklechmanik@gmail.com>
2016-08-24 01:28:45 +0000

    All tests passed

---
+++ language_detector/for_test.py
@@ -0,0 +1,10 @@
+languages = [
+    {'name': 'Spanish'},  # 0
+    {'nam': 'English'},  # 1
+    {'name': 'German'},   # 2
+]
+
+for language in languages:
+    # language {'name': 'Spanish'}
+    name = language['name']
+    print(name)
--- language_detector/main.py
+++ language_detector/main.py
@@ -6,6 +6,18 @@ from languages import LANGUAGES
 
 
 def detect_language(text, languages=LANGUAGES):
-    """Returns the detected language of given text."""
-    # implement your solution here
-    pass
+    
+    stats = {}
+    for language in languages:
+        name = language['name']
+        stats[name] = 0
+    
+    for item in text.split(' '):    #iterate through each word in text
+        for language in languages:  #iterates through each language
+            name = language['name']
+            for word in language.get('common_words'): #iterates through each common word
+                # language
+                if item == word:
+                    stats[name] += 1
+    
+    return max(stats, key=stats.get)
